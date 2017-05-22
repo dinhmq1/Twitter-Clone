ba7d3783cd6bc644e5c2b451e3a53aed757b83e3
Mike Dinh <dinhmq1@gmail.com>
2017-05-21 20:11:53 -0500

    update

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -6,19 +6,18 @@ import './SignIn.css';
 
 const SignIn = ({ signIn }) => {
   return (
-    <div className="SignInPageBg">
+
       <div className="SignIn z-depth-1">
       <div className="SignInLogo">
-      <h1>Twitter Clone</h1>
         <img src="http://app.chatterspot.com/UploadedImages/5eeb2075-47c7-4053-ad92-68ee14159d0b.png" alt="twitter logo" height="100"/>
+                    <h1>Clone</h1>
               <button
         className="BtnSignIn"
         onClick={signIn}
       >
         Sign In
-      </button>
+      </button> 
           </div>
-      </div>
     </div>
   );
 };
